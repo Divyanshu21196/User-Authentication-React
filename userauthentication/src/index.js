5cf443e72ea1b4bfb39ea164b1b930e698309bbd
Divyanshu <divy211996@gmail.com>
2023-06-28 19:24:15 +0530

    Initial setup

---
+++ userauthentication/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import  ReactDOM  from "react-dom/client";
+import App from "./App";
+import './App.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
+const el = document.getElementById('root');
+const root = ReactDOM.createRoot(el);
+
+root.render(
+    <App />
+    )
